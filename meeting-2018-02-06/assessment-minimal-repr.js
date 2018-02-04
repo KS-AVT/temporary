730f9ee7d8ea3888edfb11ef8bd9285ce471236f
torleifg <torgravels@gmail.com>
2018-02-04 20:32:50 +0100

    Create assessment-minimal-repr.js

---
+++ meeting-2018-02-06/assessment-minimal-repr.js
@@ -0,0 +1,37 @@
+{
+  "id": "efac94f3-1258-48c3-b813-f0b92aa0c236",
+  "timestamp": "2018-01-29T12:00:00+00:00",
+  "actor": {
+    "objectType": "Agent",
+    "account": {
+      "homePage": "https://docs.dataporten.no",
+      "name": "76a7a061-3c55-430d-8ee0-6f82ec42501f"
+    }
+  },
+  "verb": {
+    "id": "http://adlnet.gov/expapi/verbs/attempted"
+    "display": {
+      "en": "attempted"
+    }
+  },
+  "object": {
+    "objectType": "Activity",
+    "id": "http://www.lokus.no/open/nummer/Nummer-8/4-Tall-og-algebra/Kapitteltest",
+    "definition": {
+      "type": "http://adlnet.gov/expapi/activities/assessment"
+    }
+  },
+  "context": {
+    "contextActivities": {
+      "grouping": [
+        {
+          "objectType": "Activity",
+          "id": "http://psi.udir.no/kl06/K15201",
+          "definition": {
+            "type": "https://w3id.org/xapi/avt/activity-types/competence-aim"
+          }
+        }
+      ]
+    }
+  }
+}
