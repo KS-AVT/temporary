f4de408edad5fec0924d7b1a10f81f3723b07628
torleifg <torgravels@gmail.com>
2018-02-04 20:32:01 +0100

    Create question-minimal-repr.js

---
+++ meeting-2018-02-06/question-minimal-repr.js
@@ -0,0 +1,56 @@
+{
+  "id": "07902059-c05d-400a-bbcd-f8f1d08fbeb8",
+  "timestamp": "2018-01-29T12:10:00+00:00",
+  "actor": {
+    "objectType": "Agent",
+    "account": {
+      "homePage": "https://docs.dataporten.no",
+      "name": "76a7a061-3c55-430d-8ee0-6f82ec42501f"
+    }
+  },
+  "verb": {
+    "id": "http://adlnet.gov/expapi/verbs/answered",
+    "display": {
+      "en": "answered"
+    }
+  },
+  "object": {
+    "objectType": "Activity",
+    "id": "http://www.lokus.no/open/nummer/Nummer-8/4-Tall-og-algebra/Kapitteltest#Oppgave-1",
+    "definition": {
+      "type": "http://adlnet.gov/expapi/activities/question"
+    }
+  },
+  "context": {
+    "contextActivities": {
+      "parent": [
+        {
+          "objectType": "Activity",
+          "id": "http://www.lokus.no/open/nummer/Nummer-8/4-Tall-og-algebra/Kapitteltest",
+          "definition": {
+            "type": "http://adlnet.gov/expapi/activities/assessment"
+          }
+        }      
+      ],
+      "grouping": [
+        {
+          "objectType": "Activity",
+          "id": "http://psi.udir.no/kl06/K15201",
+          "definition": {
+            "type": "https://w3id.org/xapi/avt/activity-types/competence-aim"
+          }
+        },
+        {
+          "objectType": "Activity",
+          "id": "https://ceds.ed.gov/element/001088/#LowComplexity",
+          "definition": {
+            "type": "https://w3id.org/xapi/avt/activity-types/complexity-level"
+          }
+        }
+      ]
+    }
+  },
+  "result": {
+    "success": true
+  }
+}
